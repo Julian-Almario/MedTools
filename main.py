9f16bc4266ab96ab09ad708c1d0bf5dc26b0f479
Julian Almario <doctorz.0525@gmail.com>
2025-04-16 21:33:03 -0500

    holliday ecuacion-liquidos por hora

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 
+
 def herramientas():
     print("1. Salir")
     print("2. Calculadoras")
@@ -12,53 +13,81 @@ def calculadoras():
     while True:
         print("1. Salir")
         print("2. Liquidos en paciente quemado")
+        print("3. Liquidos en general")
         print("-----------------------------")
-    
+
+        cont = "o"
         nav = int(input("=> "))
         
         if nav == 1:
             break
 
-
 #Liquidos en paciente quemado
-        peso = 0
-        holliday = 0
-        parklnd = 0
         if nav == 2:
+            peso = 0
+            holliday = 0
+        
+        #Variables a usar en parkland
             peso = int(input("Peso(Kg): "))
             acq = int(input("ACQ: "))
-        if peso < 10:
-            holliday = peso * 100
 
-        elif peso > 11 and peso < 20:
-            val = (peso - 10) * 50  
-            holliday = 1000 + val
-
-        else:
-            val = (peso - 20) * 20
-            holliday = 1500 + val
+            if peso < 10:
+                holliday = peso * 100
+            elif peso > 11 and peso < 20:
+                val = (peso - 10) * 50  
+                holliday = 1000 + val
+            else:
+                val = (peso - 20) * 20
+                holliday = 1500 + val
 
         # parkland = (((4 * peso) * acq) + holliday) / 2
-        park1 = 4 * peso
-        park2 = park1 * acq
-        park3 = park2 + holliday
-        park4 = park3 / 2
+            park1 = 4 * peso
+            park2 = park1 * acq
+            park3 = park2 + holliday
+            park4 = park3 / 2
 
-        solu1 = park4 / 8
-        solu2 = park4 / 16
+            solu1 = park4 / 8
+            solu2 = park4 / 16
 
         #Solucion
-        print(f"Pasar {solu1}cc/hora de harmant o SS 0,9% por 8 horas, y {solu2}cc/hora por 16 horas")
-        print("SOLUCION")
-        print(f"4 * {peso} = {park1} * {acq} = {park2} + {holliday} = {park3} / 2 = {park4}")
-        print("-------------------")
-        cont = input("* Para continuar: ")
-        if cont == "*":
-            continue
-        else:
-            break
-# Administracion de liquidos
+            print(f"Pasar {solu1}cc/hora de harmant o SS 0,9% por 8 horas, y {solu2}cc/hora por 16 horas")
+            print("SOLUCION")
+            print(f"4 * {peso} = {park1} * {acq} = {park2} + {holliday} = {park3} / 2 = {park4}")
+            print("-------------------")
+            
+            #break
+            cont = input("* Para continuar: ")
+            if cont == "*":
+                continue
+            else:
+                break
 
+# Administracion de liquidos holliday
+        if nav == 3:
+
+            peso = 0
+            holliday = 0
+
+            peso = int(input("Peso(Kg): "))
+
+            if peso < 10:
+                holliday = peso * 100
+            elif peso > 11 and peso < 20:
+                val = (peso - 10) * 50
+                holliday = 1000 + val
+            else:
+                val = (peso - 20) * 20
+                holliday = 1500 + val
+
+            hollidayxhora = holliday / 24
+            print(f"administrar {hollidayxhora}cc/Hora hasta 24 horas")
+            
+            #break
+            cont = input("* Para continuar: ")
+            if cont == "*":
+                continue
+            else:
+                break
 
 def main():
 
